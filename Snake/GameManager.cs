3772a7cfff1dc6357853f4f40e0d9e68e72446af
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2018-03-17 22:11:38 +0300

    render cells

--- Snake/GameManager.cs
+++ Snake/GameManager.cs
@@ -7,6 +7,9 @@
 // Copyright (c) 2018 Yauheni Pakala
 //
 using System;
+using System.Linq;
+using Foundation;
+using UIKit;
 
 namespace Snake
 {
@@ -21,7 +24,26 @@ namespace Snake
 
         public void InitGame()
         {
-           
+            scene.playerPositions.Add(new Pos(10, 10));
+            scene.playerPositions.Add(new Pos(10, 11));
+            scene.playerPositions.Add(new Pos(10, 12));
+
+            RenderChange();
+        }
+
+        void RenderChange()
+        {
+            foreach (var cell in scene.gameArray)
+            {
+                if (scene.playerPositions.Any(x => x.X == cell.X && x.Y == cell.Y))
+                {
+                    cell.Node.FillColor = UIColor.Cyan;
+                }
+                else
+                {
+                    cell.Node.FillColor = UIColor.Clear;
+                }
+            }
         }
     }
 }
